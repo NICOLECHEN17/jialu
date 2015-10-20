e96ac130a70253727d93136ae9b74cb0027c704c
Jialu Chen <657140380@qq.com>
2015-10-20 11:09:04 +0100

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ def create
                                   :password, :password_confirmation)
         @user = User.new(secure_params)
         if @user.save
-        	flash[:success] = "Welcome to the Twitter App!" 
+          remember @user
+        	flash[:success] = "Welcome to the Sample App!"  
            redirect_to @user # Handle a successful save.
         else
             render 'new'  # Handle an unsuccessful save.     
